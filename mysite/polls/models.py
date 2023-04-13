2ab19e8ae8da1a48681216574dc845eaa49ae967
Sowmiyaa Vasan <sowmiyaavasan@Sowmiyaas-MacBook-Pro.local>
2023-04-13 14:33:14 -0600

    committing solutions for part a and b of second section

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 import datetime
-
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, unique=True)
     pub_date = models.DateTimeField('date published')
+    
     def __str__(self):
         return self.question_text
 
@@ -15,11 +15,13 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
   
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    class UniqueChoiceText:
+        unique_together = ('question', 'choice_text')
+        
     def __str__(self):
         return self.choice_text
